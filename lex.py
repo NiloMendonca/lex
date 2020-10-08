6ffa78f32fae4d5e0eb2871b90e5e34639bc1fd8
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-10-08 08:20:37 -0300

    first commit

---
+++ lex.py
@@ -0,0 +1,44 @@
+import re
+from typing import NamedTuple, Iterable
+
+
+class Token(NamedTuple):
+    kind: str
+    value: str
+
+
+def lex(code: str) -> Iterable[Token]:
+    """
+    Retorna sequência de objetos do tipo token correspondendo à análise léxica
+    da string de código fornecida.
+    """
+    array = []
+    keywords = {'IF', 'THEN', 'ENDIF', 'FOR', 'NEXT', 'GOSUB', 'RETURN'}
+    token_specification = [
+        ('NUMBER',   r'\d+(\.\d*)?'),  # Integer or decimal number
+        ('ASSIGN',   r':='),           # Assignment operator
+        ('END',      r';'),            # Statement terminator
+        ('ID',       r'[A-Za-z]+'),    # Identifiers
+        ('OP',       r'[+\-*/]'),      # Arithmetic operators
+        ('NEWLINE',  r'\n'),           # Line endings
+        ('SKIP',     r'[ \t]+'),       # Skip over spaces and tabs
+        ('MISMATCH', r'.'),            # Any other character
+    ]
+    tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+    for mo in re.finditer(tok_regex, code):
+        kind = mo.lastgroup
+        value = mo.group()
+        if kind == 'NUMBER':
+            value = float(value) if '.' in value else int(value)
+    #     elif kind == 'ID' and value in keywords:
+    #         kind = value
+        elif kind == 'NEWLINE':
+            continue
+        elif kind == 'SKIP':
+            continue
+    #     elif kind == 'MISMATCH':
+    #         raise RuntimeError(f'{value!r}')
+        array.append(Token(kind, value))
+    
+    # return [Token('INVALIDA', 'valor inválido')]
+    return array
